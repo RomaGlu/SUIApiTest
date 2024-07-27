804b0c99245b5c5b27027150ce17ba1652e20b21
Roman Hlukharou <roma.glu.69@gmail.com>
2024-07-27 16:05:02 +0300

    Initial Commit

---
+++ SUIApiTest/SUIApiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApiTestApp.swift
+//  SUIApiTest
+//
+//  Created by Роман Глухарев on 27.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SUIApiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
